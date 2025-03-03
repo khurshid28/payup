c0fe0340cde4594c0998c578021653527535b210
dohcgle <utn1002@gmail.com>
2025-03-03 16:58:25 +0500

    upd0303

--- pages/views.py
+++ pages/views.py
@@ -2,15 +2,21 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from stepform.models import Organization
+
 
 @login_required()
 def home(request):
     user = request.user
     user_groups = user.groups.all()  # Foydalanuvchi guruhlarini olish
-    print(request.build_absolute_uri("/"))
-
+    organization = Organization.objects.first()
+    if organization:
+        organization = organization
+    else:
+        organization = "Topilmadi"
     context = {
-        'user_groups': user_groups
+        'user_groups': user_groups,
+        'organization': organization,
     }
 
     return render(request, 'pages/index.html', context)
